136f290ff680466d708db1fa1c6457ba3c25db32
Loai-101 <q9g8moh@gmail.com>
2025-09-30 14:18:49 +0300

    Replace logo across all components - header, browser favicon, manifest, and social media meta tags

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -54,7 +54,7 @@ const Header = () => {
         <div className="header-logo">
           <Link to="/" className="header-logo-link">
             <img 
-              src="https://res.cloudinary.com/dvybb2xnc/image/upload/v1758792444/40e4dc9c-28ff-4fd9-83da-87e1c3d89d6d_mh1ghi.png" 
+              src="https://res.cloudinary.com/dvybb2xnc/image/upload/v1759230964/Screenshot_2025-09-30_141557_glwpx3.png" 
               alt="Expo Physiotherapy Center Logo" 
               className="header-logo-image"
             />
